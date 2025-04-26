6d85675ab0e5c0e64552c409a431a06d4a9cb950
Ömer Çevrük <omer.cevrukk@gmail.com>
2025-04-26 16:52:05 +0300

    adds .tsx extension on imports for better lsp function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import PredictionForm from './components/PredictionForm';
-import Footer from './components/Footer';
+import Header from './components/Header.tsx';
+import Hero from './components/Hero.tsx';
+import PredictionForm from './components/PredictionForm.tsx';
+import Footer from './components/Footer.tsx';
 
 const App = () => {
   return (
